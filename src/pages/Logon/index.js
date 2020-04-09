99369c372d240ca472d7a3dd2a9b8418d6abdac4
Júlio César Bueno <buenojc@outlook.com>
2020-04-08 23:46:29 -0300

    Added content to Logon (WIP)

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./styles.css";
+import { FiLogIn } from "react-icons/fi";
+import logoImg from "../../assets/logo.svg";
+import hero from "../../assets/heroes.png";
 
 export default function Logon() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+          <FiLogIn size={16} color="#e02041" />
+          <a href="/register">Não tenho cadastro</a>
+        </form>
+      </section>
+      <img src={hero} alt="Heroes" />
     </div>
   );
 }
