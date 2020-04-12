a6dd935c747c52f8ced2f85725dd58cd4008cc61
Júlio César Bueno <buenojc@outlook.com>
2020-04-12 18:19:28 -0300

    Connect the component with DB
    Now users can add new incidents directly from the front-end

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,10 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 import "./styles.css";
 import logoImg from "../../assets/logo.svg";
 
 export default function NewIncident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const ongId = localStorage.getItem("ongId");
+
+  const history = useHistory();
+
+  async function handleCreateIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post("/incidents", data, {
+        headers: {
+          authorization: ongId,
+        },
+      });
+      history.push("/profile");
+    } catch (err) {
+      alert("Não foi possível cadastrar o novo caso, tente novamente");
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -20,10 +49,22 @@ export default function NewIncident() {
             Voltar para home
           </Link>
         </section>
-        <form>
-          <input placeholder="Titulo do Caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleCreateIncident}>
+          <input
+            placeholder="Titulo do Caso"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor em reais sem virgulas"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <div className="button-group">
             <Link className="cancel-btn" to="/profile">
