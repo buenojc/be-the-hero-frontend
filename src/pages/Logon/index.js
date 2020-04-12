669817de26d8c4c26ac6abfb97f9f1dfd53672ac
Júlio César Bueno <buenojc@outlook.com>
2020-04-12 01:25:12 -0300

    Connect the logon page with API

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { FiLogIn } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import logoImg from "../../assets/logo.svg";
 import hero from "../../assets/heroes.png";
+import api from "../../services/api";
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+
+      history.push("profile");
+    } catch (err) {
+      console.log("Erro no login, tente novamente");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu Logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
